7c729ad6fe8579e9ed62302c59d8ec6093dbe727
riyaverma-tech <riyaverma6894@gmail.com>
2026-06-07 12:26:41 +0530

    Create 1_class_my_self.c

---
+++ 1_class_my_self.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+    printf("I am a girl.\n");
+    printf("My name is Riya.\n");
+    printf("I am 13 years old.\n");
+    printf("My mother name is Jyoti.\n");
+    printf("My father name is Ashwani Kumar.\n");
+    printf("My favourite fruit is mango.\n");
+    printf("I love trevling.\n");
+    printf("My hobby is singing.\n");
+    printf("I study in class 9th.\n");
+    printf("I am self obsessed.\n");
+}
