3b75de6d3aafaa82ca74cab1e55fab286eabe3be
riyaverma-tech <riyaverma6894@gmail.com>
2026-06-09 20:15:22 +0530

    Create 2_class.c

---
+++ 2_class.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 3;
+    int year = 2026;
+    int birth = 2012;
+    int luck = 7042;
+    int age = 13;
+
+    printf("age: %i\n", age);
+    printf("luck: %i\n", luck);
+    printf(";%i\n", year);
+    printf("%i\n", birth);
+    printf("number: %i\n", number);
+
+}
